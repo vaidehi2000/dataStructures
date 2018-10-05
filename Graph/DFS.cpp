d3e4c52b5e25c2ca3f9f5543516d278e66af40de
Jaimin <jaiminchaudhary42@gmail.com>
2018-10-06 01:20:47 +0530

    Made code for DFS Search + Minor change in BFS code
    Made code for Depth First Search in Graph.
    Printed one additional line in DFS file to  make output simple to understand.

--- Graph/DFS.cpp
+++ Graph/DFS.cpp
@@ -2,14 +2,13 @@
 // Date : 6-10-2018
 // Follow me on Github : https://github.com/jaimin257
 
-
 #include<bits/stdc++.h>
 
 #define ll long long
 
 using namespace std;
 
-void BFS(ll source, vector<ll>adj[] , vector<ll>visited , vector<ll>level);
+void DFS(ll source, vector<ll>adj[] , vector<ll>visited);
 
 int main()
 {
@@ -18,7 +17,6 @@ int main()
     cin>>n>>m;
     vector <ll> adj[n+1];
     vector <ll> visited(n+1,0);
-    vector <ll> level(n+1,0);
     cout<<"enter edges of the graph.\n";
     for(ll i=0;i<m;i++)
     {
@@ -28,30 +26,28 @@ int main()
     }
     cout<<"Enter source vertex for Graph : ";
     cin>>x;
-    cout<<"BFS Search Result : ";
-    BFS(x,adj,visited,level);
+    cout<<"DFS Search Result : ";
+    DFS(x,adj,visited);
     cout<<endl;
     return 0;
 }
 
-void BFS(ll source, vector<ll>adj[] , vector<ll>visited , vector<ll>level)
+void DFS(ll source, vector<ll>adj[] , vector<ll>visited)
 {
-    queue <int> q;
-    q.push(source);
+    stack <int> st;
+    st.push(source);
     visited[source] = 1;
-    level[source] = 1;
-    while(!q.empty())
+    while(!st.empty())
     {
-        ll v = q.front();
+        ll v = st.top();
         cout<<v<<" ";
-        q.pop();
+        st.pop();
         for(ll i=0;i<adj[v].size();i++)
         {
             if(visited[adj[v][i]] == 0)
             {
-                q.push(adj[v][i]);
+                st.push(adj[v][i]);
                 visited[adj[v][i]] = 1;
-                level[adj[v][i]] = level[v] + 1;
             }
         }
     }
